f8aeacb5d9ee8f04c2a9daa1b27d6fdb5acfed69
Sahil Ubale <sahilubale2510@gmail.com>
2025-06-24 18:29:04 +0530

    Day 2 Photo gallery completed

--- lib/pages/day2.dart
+++ lib/pages/day2.dart
@@ -60,6 +60,26 @@ class day2_Page extends StatelessWidget {
     'Chetna',
   ];
 
+  final List _pictures = [
+    'Photo1',
+    'Photo2',
+    'Photo3',
+    'Photo4',
+    'Photo5',
+    'Photo6',
+    'Photo7',
+    'Photo8',
+    'Photo9',
+    'Photo10',
+    'Photo11',
+    'Photo12',
+    'Photo13',
+    'Photo14',
+    'Photo15',
+    'Photo16',
+    'Photo17',
+    'Photo18',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,50 +87,75 @@ class day2_Page extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 50,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 163, 163, 163),
-                borderRadius: BorderRadius.circular(50),
-              ),
-              child: Row(
-                children: [
-                  SizedBox(width: 10),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Icon(Icons.search),
-                  ),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: TextField(
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Enter the number",
-                        labelText: "Contacts",
+          // Padding(
+          //   padding: const EdgeInsets.all(8.0),
+          //   child: Container(
+          //     height: 50,
+          //     decoration: BoxDecoration(
+          //       color: const Color.fromARGB(255, 163, 163, 163),
+          //       borderRadius: BorderRadius.circular(50),
+          //     ),
+          //     child: Row(
+          //       children: [
+          //         SizedBox(width: 10),
+          //         Padding(
+          //           padding: const EdgeInsets.all(8.0),
+          //           child: Icon(Icons.search),
+          //         ),
+          //         SizedBox(width: 10),
+          //         Expanded(
+          //           child: TextField(
+          //             keyboardType: TextInputType.text,
+          //             decoration: InputDecoration(
+          //               border: InputBorder.none,
+          //               hintText: "Enter the number",
+          //               labelText: "Contacts",
+          //             ),
+          //           ),
+          //         ),
+          //       ],
+          //     ),
+          //   ),
+          // ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 12,
+                  mainAxisSpacing: 12,
+                ),
+                itemCount: _pictures.length,
+                itemBuilder: (context, index) {
+                  return GridTile(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(),
                       ),
+                      child: Center(child: Text(_pictures[index])),
                     ),
-                  ),
-                ],
+                  );
+                },
               ),
             ),
           ),
 
-          Expanded(
-            child: ListView.builder(
-              itemCount: _number.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(_number[index]),
-                  subtitle: Text(_names[index]),
-                  leading: Icon(Icons.person),
-                  trailing: Icon(Icons.turn_right),
-                );
-              },
-            ),
-          ),
+          // Expanded(
+          //   child: ListView.builder(
+          //     itemCount: _number.length,
+          //     itemBuilder: (context, index) {
+          //       return ListTile(
+          //         title: Text(_number[index]),
+          //         subtitle: Text(_names[index]),
+          //         leading: Icon(Icons.person),
+          //         trailing: Icon(Icons.turn_right),
+          //       );
+          //     },
+          //   ),
+          // ),
         ],
       ),
     );
